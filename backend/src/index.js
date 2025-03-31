d670f57ad595618d89080ac4c93a1f8b613cf9c0
MJTheSlamDog <gomasterjay@gmail.com>
2025-03-31 15:27:35 +0200

    Signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
 
@@ -15,6 +15,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
-
-    connectDB()
+    connectDB();
 });
+
