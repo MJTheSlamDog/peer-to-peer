ac4903cdd311f3810df3ddb8e8b8161f59b29e10
MJTheSlamDog <gomasterjay@gmail.com>
2025-04-02 00:26:57 +0200

    Update profilel and check auth

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../src/models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
     try {
@@ -9,18 +9,19 @@ export const protectRoute = async (req, res, next) => {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
         if(!decoded) {
             return res.status(401).json({ message: "Unauthorized! Invalid Token" });
         }
 
-        const user = await User.findById(decoded.id).select("-password");
+        const user = await User.findById(decoded.userId).select("-password");
         if (!user) {
-            return res.status(401).json({ message: "Unauthorized! User not found" });
+            return res.status(404).json({ message: "Unauthorized! User not found" });
         };
 
         req.user = user;
         next();
+
+
     } catch (error) {
         console.log("Error in protectRoute middleware", error.message);
         res.status(500).json({ message: "Internal Server Error" });        
